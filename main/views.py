43e6193c25b86fe7b8ac5f522a95206632f6f27c
Maxim Novoselsky <relocker121@gmail.com>
2023-05-16 22:15:54 +0700

    Registration implemented

--- main/views.py
+++ main/views.py
@@ -1,24 +1,38 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
+
+from main.models import AppUser
 
 
 # Create your views here.
 
-def find_page(request: HttpRequest):
+def find_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/find.html')
 
 
-def register_page(request: HttpRequest):
-    return render(request, 'main/regist.html')
+def register_page(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return render(request, 'main/regist.html')
+
+    django_user = User.objects.create_user(
+        username=request.POST['name'],
+        email=request.POST['email'],
+        password=request.POST['password'],
+    )
+
+    AppUser.objects.create(django_user_ref=django_user)
+
+    return HttpResponse('success')
 
 
-def sign_page(request: HttpRequest):
+def sign_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/sign.html')
 
 
-def become_page(request: HttpRequest):
+def become_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/become.html')
 
 
-def index_page(request: HttpRequest):
+def index_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/main.html')
