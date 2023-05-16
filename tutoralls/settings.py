f8e59ab68d571463e397f648e9d29d0490b5f7c2
Maxim Novoselsky <relocker121@gmail.com>
2023-05-16 23:02:06 +0700

    Added docker-compose basic deployment config

--- tutoralls/settings.py
+++ tutoralls/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'tutoralls.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES = DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutoralls',
+        'USER': 'tutoralls',
+        'PASSWORD': os.environ.get("DB_PASSWORD", default='123456'),
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
@@ -117,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
