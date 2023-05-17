2735d6164e1756601bbbcd0b93d8926b574d3434
Maxim Novoselsky <relocker121@gmail.com>
2023-05-17 22:19:33 +0700

    Added backend logic for sign in page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
@@ -16,7 +17,8 @@ def register_page(request: HttpRequest) -> HttpResponse:
         return render(request, 'main/regist.html')
 
     django_user = User.objects.create_user(
-        username=request.POST['name'],
+        first_name=request.POST['name'],
+        username=request.POST['email'],
         email=request.POST['email'],
         password=request.POST['password'],
     )
@@ -27,7 +29,19 @@ def register_page(request: HttpRequest) -> HttpResponse:
 
 
 def sign_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/sign.html')
+    if request.method == 'GET':
+        return render(request, 'main/sign.html')
+
+    username = str(request.POST['email'])
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+
+    if not user:
+        return HttpResponse('Wrong login or password', status=401)
+
+    login(request, user)
+    return HttpResponse('success')
 
 
 def become_page(request: HttpRequest) -> HttpResponse:
@@ -45,8 +59,10 @@ def student_page(request: HttpRequest) -> HttpResponse:
 def teacher_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/teacher.html')
 
+
 def table_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/table.html')
+
 
 def tableStud_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/tableStud.html')
