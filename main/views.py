59e7c15be66797f83eb60b809fd0adabb64c2f3a
Maxim Novoselsky <relocker121@gmail.com>
2023-05-17 23:05:42 +0700

    Added dynamic navigation based on django context

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
+import dataclasses
+from typing import Any
+
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render
@@ -9,8 +12,27 @@ from main.models import AppUser
 
 # Create your views here.
 
+
+@dataclasses.dataclass
+class HeaderElement:
+    href: str
+    text: str
+
+
+def get_context(request: HttpRequest) -> dict[str, Any]:
+    return {
+        'header_elements': [
+            HeaderElement(href="/", text='Главная'),
+            HeaderElement(href="find", text='Найти репетитора'),
+            HeaderElement(href="become", text='Стать репетитором'),
+            HeaderElement(href="sign", text='Вход'),
+        ]
+    }
+
+
 def find_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/find.html')
+    context = get_context(request)
+    return render(request, 'main/find.html', context)
 
 
 def register_page(request: HttpRequest) -> HttpResponse:
@@ -18,7 +40,8 @@ def register_page(request: HttpRequest) -> HttpResponse:
         return HttpResponseRedirect(reverse('main:index'))
 
     if request.method == 'GET':
-        return render(request, 'main/regist.html')
+        context = get_context(request)
+        return render(request, 'main/regist.html', context)
 
     django_user = User.objects.create_user(
         first_name=request.POST['name'],
@@ -37,7 +60,9 @@ def sign_page(request: HttpRequest) -> HttpResponse:
         return HttpResponseRedirect(reverse('main:index'))
 
     if request.method == 'GET':
-        return render(request, 'main/sign.html')
+        context = get_context(request)
+
+        return render(request, 'main/sign.html', context)
 
     username = str(request.POST['email'])
     password = request.POST['password']
@@ -52,32 +77,38 @@ def sign_page(request: HttpRequest) -> HttpResponse:
 
 
 def become_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/become.html')
+    context = get_context(request)
+    return render(request, 'main/become.html', context)
 
 
 def index_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/main.html')
+    context = get_context(request)
+    return render(request, 'main/main.html', context)
 
 
 def student_page(request: HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('main:sign'))
 
-    context = {
+    context = get_context(request)
+    context.update({
         'user_first_name': request.user.first_name,
         'description': request.user.app_user.description,
-    }
+    })
 
     return render(request, 'main/student.html', context)
 
 
 def teacher_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/teacher.html')
+    context = get_context(request)
+    return render(request, 'main/teacher.html', context)
 
 
 def table_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/table.html')
+    context = get_context(request)
+    return render(request, 'main/table.html', context)
 
 
 def tableStud_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/tableStud.html')
+    context = get_context(request)
+    return render(request, 'main/tableStud.html', context)
