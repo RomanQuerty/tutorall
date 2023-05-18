3c923bc60ede446ac0c1a0cb2d07fcfac26079cf
Maxim Novoselsky <relocker121@gmail.com>
2023-05-18 23:28:10 +0700

    Added profile saving

--- main/views.py
+++ main/views.py
@@ -137,9 +137,67 @@ def index_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/main.html', context)
 
 
+@dataclasses.dataclass
+class ProfileSetting:
+    id: str
+    value: str
+    label_text: str
+
+
+def get_profile_setting(user: User) -> list[ProfileSetting]:
+    settings = [
+        ProfileSetting(
+            id='name',
+            value=user.first_name,
+            label_text='Имя',
+        ),
+        ProfileSetting(
+            id='description',
+            value=user.app_user.description,
+            label_text='Описание',
+        )
+    ]
+    if user.app_user.user_type == AppUser.STUDENT:
+        return settings
+
+    settings.extend([
+        ProfileSetting(
+            id='location',
+            value=user.app_user.location,
+            label_text='Местоположение',
+        ),
+        ProfileSetting(
+            id='subject',
+            value=user.app_user.subject,
+            label_text='Предмет',
+        ),
+    ])
+
+    return settings
+
+
+def save_profile(request: HttpRequest) -> HttpResponse:
+    user = request.user
+    app_user = user.app_user
+
+    user.first_name = request.POST['name']
+    app_user.description = request.POST['description']
+
+    if app_user.user_type == AppUser.TEACHER:
+        app_user.location = request.POST['location']
+        app_user.subject = request.POST['subject']
+
+    user.save()
+    app_user.save()
+
+
 def profile_page(request: HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('main:sign'))
+
+    if request.method == 'POST':
+        save_profile(request)
+        return HttpResponse('success')
 
     context = get_context(request)
 
@@ -152,6 +210,7 @@ def profile_page(request: HttpRequest) -> HttpResponse:
         'user_first_name': request.user.first_name,
         'description': request.user.app_user.description,
         'user_type': user_type_map[request.user.app_user.user_type],
+        'profile_settings': get_profile_setting(request.user),
     })
 
     return render(request, 'main/profile.html', context)
